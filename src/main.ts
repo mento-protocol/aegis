a6e737e5fc78ac38eb2b9596ba897a8f2e9bf1c0
boqdan <bogdan@bowd.io>
2023-07-24 09:38:35 +0300

    feat: make appengine work

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger:
+      process.env.NODE_ENV == 'production'
+        ? ['error', 'warn']
+        : ['log', 'error', 'warn', 'debug', 'verbose'],
+  });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
