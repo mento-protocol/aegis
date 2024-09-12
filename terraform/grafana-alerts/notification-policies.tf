4afef486b28dc30216bdc1c8de0672ecf942a1fb
Philip Paetz <philip.paetz@me.com>
2024-09-12 13:09:18 +0200

    feat: added grafana notification policies

---
+++ terraform/grafana-alerts/notification-policies.tf
@@ -0,0 +1,89 @@
+resource "grafana_notification_policy" "oracle_relayers" {
+  group_by      = ["alertname", "chain"]
+  contact_point = grafana_contact_point.discord_channel_catch_all.name # Default contact point
+
+  # for_each = rule.value == "celo" ? [grafana_contact_point.splunk_on_call.name, grafana_contact_point.discord_channel_oracle_relayers_prod.name] : [grafana_contact_point.discord_channel_oracle_relayers_staging.name]
+
+  policy {
+    group_wait      = "30s"
+    group_interval  = "5m"
+    repeat_interval = "4h"
+
+    # On-Call Alerts
+    policy {
+      contact_point = grafana_contact_point.splunk_on_call.name
+
+      matcher {
+        label = "severity"
+        match = "="
+        value = "page"
+      }
+
+      continue = true
+    }
+
+    # Oracle Relayer Alerts [Alfajores]
+    policy {
+      contact_point = grafana_contact_point.discord_channel_oracle_relayers_staging.name
+
+      matcher {
+        label = "service"
+        match = "="
+        value = "oracle-relayers"
+      }
+
+      matcher {
+        label = "chain"
+        match = "="
+        value = "alfajores"
+      }
+
+      continue = true
+    }
+
+    # Oracle Relayer Alerts [Celo Mainnet]
+    policy {
+      contact_point = grafana_contact_point.discord_channel_oracle_relayers_prod.name
+
+      matcher {
+        label = "service"
+        match = "="
+        value = "oracle-relayers"
+      }
+
+      matcher {
+        label = "chain"
+        match = "="
+        value = "celo"
+      }
+
+      continue = true
+    }
+
+    # Legacy Oracle Client Alerts
+    policy {
+      contact_point = "discord-alerts-oracles"
+
+      matcher {
+        label = "service"
+        match = "="
+        value = "oracles"
+      }
+
+      continue = true
+    }
+
+    # Market Making Alerts
+    policy {
+      contact_point = "Market Making Alerts"
+
+      matcher {
+        label = "service"
+        match = "="
+        value = "marketmaker"
+      }
+
+      continue = true
+    }
+  }
+}
