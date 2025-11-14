7622b0aa823d8e4f5e2b9b3daa5bdd0606337681
Philip Paetz <philip.paetz@me.com>
2025-11-14 17:45:25 +0100

    chore: linters

--- grafana-agent/template-agent.sh
+++ grafana-agent/template-agent.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Prepare agent.yaml with values from environment variables
-sed "s|GRAFANA_AGENT_ENDPOINT|$GRAFANA_AGENT_ENDPOINT|g" agent.yaml.tmpl | \
-  sed "s|GRAFANA_AGENT_USERNAME|$GRAFANA_AGENT_USERNAME|g" | \
-  sed "s|GRAFANA_AGENT_PASSWORD|$GRAFANA_AGENT_PASSWORD|g" > agent.yaml
-
+sed "s|GRAFANA_AGENT_ENDPOINT|${GRAFANA_AGENT_ENDPOINT}|g" agent.yaml.tmpl |
+	sed "s|GRAFANA_AGENT_USERNAME|${GRAFANA_AGENT_USERNAME}|g" |
+	sed "s|GRAFANA_AGENT_PASSWORD|${GRAFANA_AGENT_PASSWORD}|g" >agent.yaml
